d3351291ae18be83f55671fee07b20d47c25e2e3
Gjorgji Kirkov <gjorgjikirkov@gmail.com>
2017-12-01 17:39:10 +0100

    - added lab6 task3

--- src/mk/ukim/finki/np/lab6/task3/MatrixTest.java
+++ src/mk/ukim/finki/np/lab6/task3/MatrixTest.java
@@ -1,18 +1,16 @@
 package mk.ukim.finki.np.lab6.task3;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 class Matrix<T> {
-    private ArrayList<ArrayList<T>> matrix;
+    private T[][] matrix;
     private int m;
     private int n;
 
     public Matrix(int numRows, int numCols) {
         this.n = numRows;
         this.m = numCols;
-        matrix = new ArrayList<>();
-        initialFill(matrix, numRows, numCols);
+        matrix = (T[][]) new Object[numRows][numCols];
     }
 
     public int getNumRows() {
@@ -24,76 +22,103 @@ class Matrix<T> {
     }
 
     public T getElementAt(int row, int col) {
-        return matrix.get(row).get(col);
+        return matrix[row][col];
     }
 
     public void setElementAt(int row, int col, T value) {
-        matrix.get(row).set(col, value);
+        matrix[row][col] = value;
     }
 
     public void fill(T element) {
-        for (int i = 0; i < n; i++) {
-            ArrayList<T> col = matrix.get(i);
-            for (int j = 0; j < m; j++) {
-                col.set(j, element);
+        for (int i = 0; i < this.n; i++) {
+            for (int j = 0; j < this.m; j++) {
+                matrix[i][j] = element;
             }
-            matrix.add(col);
         }
     }
 
     public void insertRow(int row) {
-        if (row < 0 || row > n) throw new ArrayIndexOutOfBoundsException();
-
-        matrix.add(row, new ArrayList<>());
+        if (row < 0 || row > this.n) throw new ArrayIndexOutOfBoundsException();
+        T[][] newMatrix = (T[][]) new Object[this.n + 1][this.m];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < this.m; j++) {
+                newMatrix[i][j] = this.matrix[i][j];
+            }
+        }
+        for (int i = row; i < this.n; i++) {
+            for (int j = 0; j < this.m; j++) {
+                newMatrix[i + 1][j] = matrix[i][j];
+            }
+        }
+        this.matrix = newMatrix;
+        this.n++;
     }
 
     public void deleteRow(int row) {
-        if (row < 0 || row > n) throw new ArrayIndexOutOfBoundsException();
-
-        matrix.remove(row);
+        if (row < 0 || row > this.n) throw new ArrayIndexOutOfBoundsException();
+        T[][] newMatrix = (T[][]) new Object[this.n - 1][this.m];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < this.m; j++) {
+                newMatrix[i][j] = this.matrix[i][j];
+            }
+        }
+        for (int i = row; i < this.n - 1; i++) {
+            for (int j = 0; j < this.m; j++) {
+                newMatrix[i][j] = this.matrix[i + 1][j];
+            }
+        }
+        this.matrix = newMatrix;
+        this.n--;
     }
 
     public void insertColumn(int col) {
-
+        if (col < 0 || col > this.m) throw new ArrayIndexOutOfBoundsException();
+        T[][] newMatrix = (T[][]) new Object[this.n][this.m + 1];
+        for (int i = 0; i < this.n; i++) {
+            for (int j = 0; j < col; j++) {
+                newMatrix[i][j] = this.matrix[i][j];
+            }
+        }
+        for (int i = 0; i < this.n; i++) {
+            for (int j = col; j < this.m; j++) {
+                newMatrix[i][j + 1] = matrix[i][j];
+            }
+        }
+        this.matrix = newMatrix;
+        this.m++;
     }
 
     public void deleteColumn(int col) {
-
+        if (col < 0 || col > this.m) throw new ArrayIndexOutOfBoundsException();
+        T[][] newMatrix = (T[][]) new Object[this.n][this.m - 1];
+        for (int i = 0; i < this.n; i++) {
+            for (int j = 0; j < col; j++) {
+                newMatrix[i][j] = this.matrix[i][j];
+            }
+        }
+        for (int i = 0; i < this.n; i++) {
+            for (int j = col; j < this.m - 1; j++) {
+                newMatrix[i][j] = this.matrix[i][j + 1];
+            }
+        }
+        this.matrix = newMatrix;
+        this.m--;
     }
 
     public void resize(int rows, int cols) {
-        boolean bigger = false;
-        if (this.n < rows || this.m < cols) {
-            bigger = true;
-        }
-
-        System.out.println(bigger);
-        if (bigger) {
-            for (int i = 0; i < rows; i++) {
-                ArrayList<T> listCol;
-                if (i > this.n) {
-                    listCol = new ArrayList<>();
+        T[][] newMatrix = (T[][]) new Object[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (i >= n || j >= m) {
+                    newMatrix[i][j] = null;
                 } else {
-                    listCol = matrix.get(i);
+                    newMatrix[i][j] = matrix[i][j];
                 }
-                for (int j = this.m; j < cols; j++) {
-                    listCol.add(j, null);
-                }
-                matrix.add(listCol);
             }
-        } else {
-
         }
-    }
-
-    private void initialFill(ArrayList<ArrayList<T>> matrix, int numRows, int numCols) {
-        for (int i = 0; i < numRows; i++) {
-            ArrayList<T> cols = new ArrayList<>(numCols);
-            for (int j = 0; j < numCols; j++) {
-                cols.add(null);
-            }
-            matrix.add(cols);
-        }
+        this.n = rows;
+        this.m = cols;
+        this.matrix = newMatrix;
     }
 }
 
@@ -102,40 +127,40 @@ public class MatrixTest {
     public static void main(String[] args) {
         Scanner jin = new Scanner(System.in);
         int t = jin.nextInt();
-        if ( t == 0 ) {
+        if (t == 0) {
             int r = jin.nextInt();
             int c = jin.nextInt();
-            Matrix<Integer> matrix = new Matrix<Integer>(r,c);
+            Matrix<Integer> matrix = new Matrix<Integer>(r, c);
             print(matrix);
         }
-        if ( t == 1 ) {
+        if (t == 1) {
             int r = jin.nextInt();
             int c = jin.nextInt();
-            Matrix<Integer> matrix = new Matrix<Integer>(r,c);
-            for ( int i = 0 ; i < r ; ++i ) {
-                for ( int k = 0 ; k < c ; ++k ) {
+            Matrix<Integer> matrix = new Matrix<Integer>(r, c);
+            for (int i = 0; i < r; ++i) {
+                for (int k = 0; k < c; ++k) {
                     matrix.setElementAt(i, k, jin.nextInt());
                 }
             }
             print(matrix);
         }
-        if ( t == 2 ) {
+        if (t == 2) {
             int r = jin.nextInt();
             int c = jin.nextInt();
-            Matrix<String> matrix = new Matrix<String>(r,c);
-            for ( int i = 0 ; i < r ; ++i ) {
-                for ( int k = 0 ; k < c ; ++k ) {
+            Matrix<String> matrix = new Matrix<String>(r, c);
+            for (int i = 0; i < r; ++i) {
+                for (int k = 0; k < c; ++k) {
                     matrix.setElementAt(i, k, jin.next());
                 }
             }
             print(matrix);
         }
-        if ( t == 3 ) {
+        if (t == 3) {
             int r = jin.nextInt();
             int c = jin.nextInt();
-            Matrix<String> matrix = new Matrix<String>(r,c);
-            for ( int i = 0 ; i < r ; ++i ) {
-                for ( int k = 0 ; k < c ; ++k ) {
+            Matrix<String> matrix = new Matrix<String>(r, c);
+            for (int i = 0; i < r; ++i) {
+                for (int k = 0; k < c; ++k) {
                     matrix.setElementAt(i, k, jin.next());
                 }
             }
@@ -145,12 +170,12 @@ public class MatrixTest {
             print(matrix);
             int ir = jin.nextInt();
             matrix.insertRow(ir);
-            for ( int k = 0 ; k < c ; ++k ) {
+            for (int k = 0; k < c; ++k) {
                 matrix.setElementAt(ir, k, jin.next());
             }
             ir = jin.nextInt();
             matrix.insertRow(ir);
-            for ( int k = 0 ; k < c ; ++k ) {
+            for (int k = 0; k < c; ++k) {
                 matrix.setElementAt(ir, k, jin.next());
             }
             print(matrix);
@@ -159,22 +184,22 @@ public class MatrixTest {
             print(matrix);
             int ic = jin.nextInt();
             matrix.insertColumn(ir);
-            for ( int i = 0 ; i < r ; ++i ) {
+            for (int i = 0; i < r; ++i) {
                 matrix.setElementAt(i, ic, jin.next());
             }
             ic = jin.nextInt();
             matrix.insertColumn(ic);
-            for ( int i = 0 ; i < r ; ++i ) {
+            for (int i = 0; i < r; ++i) {
                 matrix.setElementAt(i, ic, jin.next());
             }
             print(matrix);
         }
-        if ( t == 4 ) {
+        if (t == 4) {
             int r = jin.nextInt();
             int c = jin.nextInt();
-            Matrix<Integer> matrix = new Matrix<Integer>(r,c);
-            for ( int i = 0 ; i < r ; ++i ) {
-                for ( int k = 0 ; k < c ; ++k ) {
+            Matrix<Integer> matrix = new Matrix<Integer>(r, c);
+            for (int i = 0; i < r; ++i) {
+                for (int k = 0; k < c; ++k) {
                     matrix.setElementAt(i, k, jin.nextInt());
                 }
             }
@@ -188,23 +213,24 @@ public class MatrixTest {
         }
     }
 
-    public static void print ( Matrix<?> m ) {
-        int r = m.getNumRows();int c = m.getNumColumns();
-        System.out.println("  "+r+" x "+c);
+    public static void print(Matrix<?> m) {
+        int r = m.getNumRows();
+        int c = m.getNumColumns();
+        System.out.println("  " + r + " x " + c);
         System.out.print("    ");
-        for ( int k = 0 ; k < c ; ++k ) {
-            System.out.print(k+"    ");
+        for (int k = 0; k < c; ++k) {
+            System.out.print(k + "    ");
         }
         System.out.println();
         System.out.print("  ");
-        for ( int k = 0 ; k < c ; ++k ) {
+        for (int k = 0; k < c; ++k) {
             System.out.print("-----");
         }
         System.out.println();
-        for ( int i = 0 ; i < r ; ++i ) {
-            System.out.print(i+"|");
-            for ( int k = 0 ; k < c ; ++k ) {
-                if ( k > 0 ) System.out.print(" ");
+        for (int i = 0; i < r; ++i) {
+            System.out.print(i + "|");
+            for (int k = 0; k < c; ++k) {
+                if (k > 0) System.out.print(" ");
                 System.out.print(m.getElementAt(i, k));
             }
             System.out.println();
