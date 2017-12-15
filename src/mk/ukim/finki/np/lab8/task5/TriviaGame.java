1b1b34fc2a40ee06a6fc1a147cbb14b7295ec536
Gjorgji Kirkov <gjorgjikirkov@gmail.com>
2017-12-15 13:23:23 +0100

    - added lab 8 task 5

--- src/mk/ukim/finki/np/lab8/task5/TriviaGame.java
+++ src/mk/ukim/finki/np/lab8/task5/TriviaGame.java
@@ -1,117 +1,137 @@
 package mk.ukim.finki.np.lab8.task5;
 
-import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
-class TriviaQuestion {
+abstract class TriviaQuestion {
+    private String question;        // Actual question
+    private String answer;        // Answer to question
+    private int value;            // Point value of question
 
-    public static final int TRUEFALSE = 0;
-    public static final int FREEFORM = 1;
-    public String question;		// Actual question
-    public String answer;		// Answer to question
-    public int value;			// Point value of question
-    public int type;			// Question type, TRUEFALSE or FREEFORM
-
-    public TriviaQuestion() {
-        question = "";
-        answer = "";
-        value = 0;
-        type = FREEFORM;
+    public TriviaQuestion(String question, String answer, int value) {
+        this.question = question;
+        this.answer = answer;
+        this.value = value;
     }
 
-    public TriviaQuestion(String q, String a, int v, int t) {
-        question = q;
-        answer = a;
-        value = v;
-        type = t;
+    protected String getQuestion() {
+        return question;
     }
+
+    protected String getAnswer() {
+        return answer;
+    }
+
+    protected int getValue() {
+        return value;
+    }
+
+    abstract protected boolean validateAnswer(String answer);
 }
 
-class TriviaData {
 
-    private ArrayList<TriviaQuestion> data;
-
-    public TriviaData() {
-        data = new ArrayList<TriviaQuestion>();
+class TrueFalseTriviaQuestion extends TriviaQuestion {
+    public TrueFalseTriviaQuestion(String question, String answer, int value) {
+        super(question, answer, value);
     }
 
-    public void addQuestion(String q, String a, int v, int t) {
-        TriviaQuestion question = new TriviaQuestion(q, a, v, t);
-        data.add(question);
-    }
-
-    public void showQuestion(int index) {
-        TriviaQuestion q = data.get(index);
-        System.out.println("Question " + (index + 1) + ".  " + q.value + " points.");
-        if (q.type == TriviaQuestion.TRUEFALSE) {
-            System.out.println(q.question);
-            System.out.println("Enter 'T' for true or 'F' for false.");
-        } else if (q.type == TriviaQuestion.FREEFORM) {
-            System.out.println(q.question);
+    @Override
+    public boolean validateAnswer(String answer) {
+        if (super.getAnswer().charAt(0) == answer.charAt(0)) {
+            System.out.println("That is correct!  You get " + super.getValue() + " points.");
+            return true;
+        } else {
+            System.out.println("Wrong, the correct answer is " + super.getAnswer());
+            return false;
         }
     }
 
-    public int numQuestions() {
-        return data.size();
+    @Override
+    public String toString() {
+        return super.getQuestion() +
+                "\n" +
+                "Enter 'T' for true or 'F' for false.";
+    }
+}
+
+
+class FreeFormTriviaQuestion extends TriviaQuestion {
+    public FreeFormTriviaQuestion(String question, String answer, int value) {
+        super(question, answer, value);
     }
 
-    public TriviaQuestion getQuestion(int index) {
-        return data.get(index);
+    @Override
+    public boolean validateAnswer(String answer) {
+        if (super.getAnswer().toLowerCase().equals(answer.toLowerCase())) {
+            System.out.println("That is correct!  You get " + super.getValue() + " points.");
+            return true;
+        } else {
+            System.out.println("Wrong, the correct answer is " + super.getAnswer());
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.getQuestion();
     }
 }
 
 public class TriviaGame {
 
-    public TriviaData questions;	// Questions
+    public List<TriviaQuestion> questions;    // Questions
 
     public TriviaGame() {
         // Load questions
-        questions = new TriviaData();
-        questions.addQuestion("The possession of more than two sets of chromosomes is termed?",
-                "polyploidy", 3, TriviaQuestion.FREEFORM);
-        questions.addQuestion("Erling Kagge skiied into the north pole alone on January 7, 1993.",
-                "F", 1, TriviaQuestion.TRUEFALSE);
-        questions.addQuestion("1997 British band that produced 'Tub Thumper'",
-                "Chumbawumba", 2, TriviaQuestion.FREEFORM);
-        questions.addQuestion("I am the geometric figure most like a lost parrot",
-                "polygon", 2, TriviaQuestion.FREEFORM);
-        questions.addQuestion("Generics were introducted to Java starting at version 5.0.",
-                "T", 1, TriviaQuestion.TRUEFALSE);
+        questions = new ArrayList<>();
+        questions.add(new FreeFormTriviaQuestion(
+                "The possession of more than two sets of chromosomes is termed?",
+                "polyploidy",
+                3));
+        questions.add(new TrueFalseTriviaQuestion(
+                "Erling Kagge skiied into the north pole alone on January 7, 1993.",
+                "F",
+                1));
+        questions.add(new FreeFormTriviaQuestion(
+                "1997 British band that produced 'Tub Thumper'",
+                "Chumbawumba",
+                2));
+        questions.add(new FreeFormTriviaQuestion(
+                "I am the geometric figure most like a lost parrot",
+                "polygon",
+                2));
+        questions.add(new TrueFalseTriviaQuestion(
+                "Generics were introducted to Java starting at version 5.0.",
+                "T",
+                1));
     }
+
+    private void showQuestion(int index) {
+        TriviaQuestion question = questions.get(index);
+        System.out.println("Question " + (index + 1) + ".  " + question.getValue() + " points.");
+        System.out.println(question.toString());
+    }
+
     // Main game loop
 
     public static void main(String[] args) {
-        int score = 0;			// Overall score
-        int questionNum = 0;	// Which question we're asking
+        int score = 0;            // Overall score
         TriviaGame game = new TriviaGame();
         Scanner keyboard = new Scanner(System.in);
         // Ask a question as long as we haven't asked them all
-        while (questionNum < game.questions.numQuestions()) {
+        for (int i = 0; i < game.questions.size(); i++) {
             // Show question
-            game.questions.showQuestion(questionNum);
+            game.showQuestion(i);
             // Get answer
             String answer = keyboard.nextLine();
             // Validate answer
-            TriviaQuestion q = game.questions.getQuestion(questionNum);
-            if (q.type == TriviaQuestion.TRUEFALSE) {
-                if (answer.charAt(0) == q.answer.charAt(0)) {
-                    System.out.println("That is correct!  You get " + q.value + " points.");
-                    score += q.value;
-                } else {
-                    System.out.println("Wrong, the correct answer is " + q.answer);
-                }
-            } else if (q.type == TriviaQuestion.FREEFORM) {
-                if (answer.toLowerCase().equals(q.answer.toLowerCase())) {
-                    System.out.println("That is correct!  You get " + q.value + " points.");
-                    score += q.value;
-                } else {
-                    System.out.println("Wrong, the correct answer is " + q.answer);
-                }
+            TriviaQuestion q = game.questions.get(i);
+            if (q.validateAnswer(answer)) {
+                score += q.getValue();
             }
             System.out.println("Your score is " + score);
-            questionNum++;
         }
         System.out.println("Game over!  Thanks for playing!");
     }
